9d9aed1d8cd66c5efdd7b0b0a576492f84ae0e7f
nitzanto <nitzan16001@gmail.com>
2023-09-23 20:14:53 +0300

    * Created abstract repo for creating new documents

---
+++ libs/common/src/database/abstract.repository.ts
@@ -0,0 +1,17 @@
+import { AbstractDocument } from '@app/common/database/abstract.schema';
+import { Model, Types } from 'mongoose';
+import { Logger } from '@nestjs/common';
+
+export abstract class AbstractRepository<TDocument extends AbstractDocument> {
+  protected abstract readonly logger: Logger;
+
+  constructor(protected readonly model: Model<TDocument>) {}
+
+  async create(document: Omit<TDocument, '_id'>): Promise<TDocument> {
+    const createdDocument = new this.model({
+      ...document,
+      _id: new Types.ObjectId(),
+    });
+    return (await createdDocument.save()).toJSON() as unknown as TDocument;
+  }
+}
