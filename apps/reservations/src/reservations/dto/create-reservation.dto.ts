cc37623362ea91d32243d6933a4fe858fc3b4c1d
nitzanto <nitzan16001@gmail.com>
2023-09-24 00:46:57 +0300

    * Validation for creating reservation model

--- apps/reservations/src/reservations/dto/create-reservation.dto.ts
+++ apps/reservations/src/reservations/dto/create-reservation.dto.ts
@@ -1,11 +1,18 @@
 import { Prop } from '@nestjs/mongoose';
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateReservationDto {
+  @IsDate()
   startDate: Date;
 
+  @IsDate()
   endDate: Date;
 
+  @IsString()
+  @IsNotEmpty()
   placeId: string;
 
+  @IsString()
+  @IsNotEmpty()
   invoiceId: string;
 }
